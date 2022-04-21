d0c4666996cabe8bc081c28230605e18800571b5
Isabelle Araújo <isabelletecn@gmail.com>
2022-04-21 02:26:01 -0300

    edited scriptsdb

--- scriptsdb.py
+++ scriptsdb.py
@@ -3,17 +3,18 @@ import database as db
 import pandas as pd
 import csv
 
-# tables = ['qualis', 'researchers']
+tables = ['qualis', 'researchers']
 
-# # Dropando a tabelas caso elas já existam
-# for table in tables:
-#     sql = 'DROP TABLE {}'.format(table)
-#     db.create_db(sql)
+# Dropando a tabelas caso elas já existam
+for table in tables:
+    sql = 'DROP TABLE {}'.format(table)
+    db.create_db(sql)
 
 # Criando a tabelas
 sql = '''CREATE TABLE researchers( 
         nome_completo       VARCHAR(150), 
         citacao             VARCHAR(75),
+        citacao_nCompleto   VARCHAR(75),
         categoria           VARCHAR(25),
         PRIMARY KEY (nome_completo, citacao)
         )'''
@@ -30,26 +31,23 @@ sql = '''CREATE TABLE qualis(
 db.create_db(sql)
 
 # inserindo dados na tabela
-## researchers 
+# researchers 
 with open('PPCICresearchers.json', encoding='utf-8') as fileR:
     researchers = json.load(fileR)
     
     for category in researchers:
         for researcher in researchers[category]:
             for citations in researchers[category][researcher]:
-                sql = "INSERT INTO researchers (nome_completo, citacao, categoria) VALUES ('{}', '{}', '{}')".format(researcher, citations, category[6:])
+                cit = ', '.join(citations)
+                citNC = citations[1]+ ' ' + citations[0]
+                sql = "INSERT INTO researchers (nome_completo, citacao, citacao_nCompleto, categoria) VALUES ('{}', '{}', '{}')".format(researcher, cit, citNC, category[6:])
                 db.insert_db(sql)
 
-## qualis
+# qualis
 with open('qualis.csv', encoding='utf-8') as fileQ:
     table = csv.reader(fileQ)
     
     for line in table:
         if (line[0]!='ISSN'):
-            sql = "INSERT INTO Qualis (issn, nome, qualis) VALUES ('{}', '{}', '{}')".format(line[0], line[1].replace("'",""), line[2])
+            sql = "INSERT INTO qualis (issn, nome, qualis) VALUES ('{}', '{}', '{}')".format(line[0], line[1].replace("'",""), line[2])
             db.insert_db(sql)
-
-
-df_bd = pd.DataFrame(db.consult_db("SELECT * FROM researchers"), columns=["id", "issn", "nome", "qualis"])
-# df_bd.head()
-print(df_bd.head())
