6dd2fd66374f97960464eb8af059e17644f16b56
Isabelle Araújo <isabelletecn@gmail.com>
2022-05-30 16:49:38 -0300

    update interface

--- interface.py
+++ interface.py
@@ -2,33 +2,34 @@ from main import main
 from tkinter import *
 from tkinter import ttk
 
-root = Tk()
-root.title('PONDOC')
-root.geometry('+400+200')
-root.resizable(False, False)
+def interface():
+    root = Tk()
+    root.title('PONDOC')
+    root.geometry('+150+100')
+    root.resizable(False, False)
 
-window = ttk.Frame(root, padding=10)
-window.grid()
+    window = ttk.Frame(root, padding=10)
+    window.grid()
+    # column 1
+    column1 = ttk.Frame(window, padding=10)
+    column1.grid(column=0, row=0)
 
-column1 = ttk.Frame(window, padding=10)
-column1.grid(column=0, row=0)
+    t1 = ttk.Label(column1, text='Ano:').grid(column=0, row=0)
+
+    ano = StringVar()
+    ano.set(2021)   #default
+    anoMenu = OptionMenu(column1, ano, *range(2017, 2022))
+    anoMenu.grid(column=1, row=0)
+
+    # column 2
+    column2 = ttk.Frame(window, padding=10)
+    column2.grid(column=1, row=0)
+
+    advance = ttk.Button(column2, text='Avançar', command=lambda: [main(ano.get()), window.destroy()])
+    advance.grid()
+    cancel = ttk.Button(column2, text='Cancelar', command=root.destroy).grid()
+
+    root.mainloop()
 
 
-period = ttk.Label(column1, text='Periódicos').grid()
-text1 = ttk.Label(column1, text='url:').grid(column=0, row=1)
-urlp = StringVar()
-textboxp = ttk.Entry(column1, textvariable=urlp, width=70).grid(column=1, row=1)
-
-confer = ttk.Label(column1, text='Conferências').grid()
-text2 = ttk.Label(column1, text='url:').grid(column=0, row=3)
-urlc = StringVar()
-textboxc= ttk.Entry(column1, textvariable=urlc, width=70).grid(column=1, row=3)
-
-column2 = ttk.Frame(window, padding=10)
-column2.grid(column=1, row=0)
-
-advance = ttk.Button(column2, text='Avançar', command=lambda: main(urlp.get(), urlc.get()))
-advance.grid()
-cancel = ttk.Button(column2, text='Cancelar', command=root.destroy).grid()
-
-root.mainloop()
+interface()
