a37a8b75dd3df132b1ef994735a6e60a89538616
Isabelle Araújo <isabelletecn@gmail.com>
2022-06-11 17:56:26 -0300

    1.03

--- scriptsdb.py
+++ scriptsdb.py
@@ -3,27 +3,35 @@ import database as db
 import pandas as pd
 import csv
 
-tables = ['qualis', 'researchers']
+tables = ['researchers', 'students', 'qualis']
 
 # Dropando a tabelas caso elas já existam
 for table in tables:
-    sql = 'DROP TABLE {}'.format(table)
+    sql = f'DROP TABLE IF EXISTS {table}'
     db.create_db(sql)
 
 # Criando a tabelas
 sql = '''CREATE TABLE researchers( 
         nome                VARCHAR(255), 
         referencia          VARCHAR(50),
-        categoria           VARCHAR(25),
-        PRIMARY KEY (nome, referencia, categoria)
+        PRIMARY KEY (nome, referencia)
+        )'''
+
+db.create_db(sql)
+
+sql = '''CREATE TABLE students( 
+        nome                VARCHAR(255), 
+        referencia1          VARCHAR(50),
+        referencia2          VARCHAR(50),
+        PRIMARY KEY (nome, referencia1, referencia2)
         )'''
 
 db.create_db(sql)
 
 sql = '''CREATE TABLE qualis(
-        issn          VARCHAR(10), 
+        issn          VARCHAR(9), 
         nome          VARCHAR(255), 
-        qualis        VARCHAR(3),
+        qualis        VARCHAR(2),
         PRIMARY KEY (issn, nome)
         )'''
 
@@ -37,14 +45,24 @@ with open('PPCICresearchers.json', encoding='utf-8') as fileR:
     for category in researchers:
         for researcher in researchers[category]:
             for citations in researchers[category][researcher]:
-                sql = "INSERT INTO researchers (nome, referencia, categoria) VALUES ('{}', '{}', '{}')".format(researcher.upper(), citations.upper(), category[6:].upper())
+                sql = f"INSERT INTO researchers (nome, referencia) VALUES ('{researcher.upper()}', '{citations.upper()}')"
                 db.insert_db(sql)
+
+# students
+with open('discentes.csv', encoding='utf-8') as arq:
+    table = csv.reader(arq)
+    
+    for line in table:
+        if (line[0]!='nome'):
+            sql = f"INSERT INTO students (nome, referencia1, referencia2) VALUES ('{line[0].upper()}', '{line[1].upper()}', '{line[2].upper()}')"
+            db.insert_db(sql)
 
 # qualis
 with open('qualis.csv', encoding='utf-8') as fileQ:
     table = csv.reader(fileQ)
     
     for line in table:
-        if (line[0]!='ISSN'):
-            sql = "INSERT INTO qualis (issn, nome, qualis) VALUES ('{}', '{}', '{}')".format(line[0], line[1].replace("'",""), line[2])
+        if 'ISSN' not in line:
+            line[1] = line[1].replace("'", '"')
+            sql = f"INSERT INTO qualis (issn, nome, qualis) VALUES ('{line[0]}', '{line[1]}', '{line[2]}')"
             db.insert_db(sql)
