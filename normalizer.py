0d772e9568ec6d6a16ea51703400d258d933766e
Isabelle Araújo <isabelletecn@gmail.com>
2022-06-11 22:23:57 -0300

    normalizer 2.0

--- normalizer.py
+++ normalizer.py
@@ -1,18 +1,25 @@
 import database as db
 import pandas as pd
 
+# retorna lista com nome dos docentes e discentes que participaram normalizada. (SILVA, M. P. -> MARIA PEREIRA DA SILVA)
 def normalizer(l):
-    n = []
+    doc, dis = [], []
     for ref in l:
         surnameref = ref[0].strip() + ', '
-        ref.pop(0)
-        nameref = (surnameref+ ' '.join(ref)).upper()
+        nameref = (surnameref+ ' '.join(ref[1:])).upper()
 
         docentes=pd.DataFrame(db.consult_db("SELECT referencia FROM researchers"))
-
-        for i in docentes[0]:
-            if nameref == i:
-                sql = f"SELECT nome FROM researchers WHERE referencia = '{i}'"
+        for ref in docentes[0]:
+            if nameref == ref:
+                sql = f"SELECT nome FROM researchers WHERE referencia = '{ref}'"
                 fullname = pd.DataFrame(db.consult_db(sql))[0][0]
-                n.append(fullname)
-    return n
+                doc.append(fullname)
+
+        discentes=pd.DataFrame(db.consult_db("SELECT referencia FROM students"))
+        for ref in discentes[0]:
+            if nameref == ref:
+                sql = f"SELECT nome FROM students WHERE referencia = '{ref}'"
+                fullname = pd.DataFrame(db.consult_db(sql))[0][0]
+                dis.append(fullname)
+              
+    return doc, dis
