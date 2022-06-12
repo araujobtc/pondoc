a92d5eab5982ba6e2f5bc78e78825526bb50c4f7
Isabelle Araújo <isabelletecn@gmail.com>
2022-06-11 22:21:27 -0300

    scripts 1.04

--- scriptsdb.py
+++ scriptsdb.py
@@ -1,6 +1,5 @@
 import json
 import database as db
-import pandas as pd
 import csv
 
 tables = ['researchers', 'students', 'qualis']
@@ -21,9 +20,8 @@ db.create_db(sql)
 
 sql = '''CREATE TABLE students( 
         nome                VARCHAR(255), 
-        referencia1          VARCHAR(50),
-        referencia2          VARCHAR(50),
-        PRIMARY KEY (nome, referencia1, referencia2)
+        referencia          VARCHAR(50),
+        PRIMARY KEY (nome, referencia)
         )'''
 
 db.create_db(sql)
@@ -39,30 +37,27 @@ db.create_db(sql)
 
 # inserindo dados na tabela
 # researchers 
-with open('PPCICresearchers.json', encoding='utf-8') as fileR:
-    researchers = json.load(fileR)
+with open('PPCICresearchers.json', encoding='utf-8') as arq:
+    researchers = json.load(arq)
     
     for category in researchers:
         for researcher in researchers[category]:
             for citations in researchers[category][researcher]:
-                sql = f"INSERT INTO researchers (nome, referencia) VALUES ('{researcher.upper()}', '{citations.upper()}')"
-                db.insert_db(sql)
+                db.insert_db(f"INSERT INTO researchers (nome, referencia) VALUES ('{researcher.upper()}', '{citations.upper()}')")
 
 # students
 with open('discentes.csv', encoding='utf-8') as arq:
-    table = csv.reader(arq)
+    discentes = csv.reader(arq)
     
-    for line in table:
-        if (line[0]!='nome'):
-            sql = f"INSERT INTO students (nome, referencia1, referencia2) VALUES ('{line[0].upper()}', '{line[1].upper()}', '{line[2].upper()}')"
-            db.insert_db(sql)
-
+    for line in discentes:
+        if 'nome' not in line:
+            db.insert_db(f"INSERT INTO students (nome, referencia) VALUES ('{line[0].upper()}', '{line[1].upper()}')")
+            db.insert_db(f"INSERT INTO students (nome, referencia) VALUES ('{line[0].upper()}', '{line[2].upper()}')")
 # qualis
-with open('qualis.csv', encoding='utf-8') as fileQ:
-    table = csv.reader(fileQ)
+with open('qualis.csv', encoding='utf-8') as arq:
+    qualis = csv.reader(arq)
     
-    for line in table:
+    for line in qualis:
         if 'ISSN' not in line:
             line[1] = line[1].replace("'", '"')
-            sql = f"INSERT INTO qualis (issn, nome, qualis) VALUES ('{line[0]}', '{line[1]}', '{line[2]}')"
-            db.insert_db(sql)
+            db.insert_db(f"INSERT INTO qualis (issn, nome, qualis) VALUES ('{line[0]}', '{line[1]}', '{line[2]}')")
