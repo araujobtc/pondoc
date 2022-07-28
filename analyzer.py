3d57633525ca82ba869d9826098606d7251f7c41
Isabelle Araújo <isabelletecn@gmail.com>
2022-07-28 00:25:30 -0300

    analyzer 3.0

--- analyzer.py
+++ analyzer.py
@@ -27,7 +27,7 @@ def parseRef(citation_str, debug = False):
     unicodePrintables = ''.join(chr(c) for c in range(sys.maxunicode) 
                                             if not chr(c) in [' ', ';', ',', '.'])
 
-    unicodePrintablesJournal = ''.join(chr(c) for c in range(sys.maxunicode) 
+    unicodePrintablesConferJournal = ''.join(chr(c) for c in range(sys.maxunicode) 
                                             if not chr(c) in [';', '.'])
 
     non_abbreviated_name = Word(unicodePrintables)
@@ -45,14 +45,17 @@ def parseRef(citation_str, debug = False):
 
     author_list = (delimitedList(author, delim = ';') + Literal('.').suppress()) 
 
-    sentence = (OneOrMore(Word(unicodePrintables, excludeChars='.'), stopOn=Literal('.')))
-
+    sentence = (OneOrMore(Word(unicodePrintablesConferJournal, excludeChars='.'), stopOn=Literal('.')))
+    
     title = sentence('Title')
     title = title.setParseAction(makeDecoratingParseAction("title"))
 
-    sentenceJournal = OneOrMore(Word(unicodePrintablesJournal, excludeChars='.'), stopOn=Literal('.'))
-    journal = sentenceJournal('Journal')
-    journal = journal.setParseAction(makeDecoratingParseAction("journal"))
+    conference = Literal('EM: ').suppress() + OneOrMore(Word(unicodePrintablesConferJournal, excludeChars='.,'), stopOn=Literal('.'))
+    journal = OneOrMore(Word(unicodePrintablesConferJournal, excludeChars='.'), stopOn=Literal('.'))
+    
+    conferjournal_name = (conference|journal)
+    conferjournal = conferjournal_name('conferjournal_name') + (Literal(',').suppress()|Literal('.').suppress())
+    conferjournal = conferjournal.setParseAction(makeDecoratingParseAction("conference_journal"))
 
     valid_year = Word(nums, exact=4) + Literal('.').suppress()
 
@@ -63,82 +66,7 @@ def parseRef(citation_str, debug = False):
     remaining_stuff = remaining_stuff.setParseAction(makeDecoratingParseAction("Remaining"))
 
     valid_qualis = (Word(alphanums, exact=2) | Literal('C')) + ZeroOrMore(Word(printables, excludeChars='.'))
-    ni_qualis =  (Literal('Não identificado') + OneOrMore(Word(printables, excludeChars='.')))
-
-    qualis = (valid_qualis | ni_qualis + Literal('.').suppress())
-    qualis = qualis.setParseAction(makeDecoratingParseAction("qualis"))
-
-   
-    if debug:
-        # to track the matching expressions
-        non_abbreviated_name.setName("non_abbreviated_name").setDebug()
-        abbreviated_name.setName("abbreviated_name").setDebug()
-        author.setName("author").setDebug()
-        author_list.setName("author_list").setDebug()
-        sentence.setName("Sentence").setDebug()
-        title.setName("title").setDebug()
-        journal.setName("Journal").setDebug()
-        year.setName("year").setDebug()
-        remaining_stuff.setName("Remaining").setDebug()
-        qualis.setName("qualis").setDebug()
-    
-    citation = (author_list('AuthorLst') + 
-                title + 
-                Literal('.') + 
-                journal + 
-                Literal('.') + 
-                remaining_stuff +
-                year +
-                qualis)
-
-    result = citation.parseString(citation_str)
-
-    return result
-
-def parseConferenceRef(citation_str, debug = False):
-    author_name_before_comma = CharsNotIn(',')
-    author_name_before_comma.setParseAction(makeDecoratingParseAction("author_name_before_comma"))
-    abbreviated_name = Combine(Word(alphas.upper(), exact=1) + '.'|' ')
-
-    unicodePrintables = ''.join(chr(c) for c in range(sys.maxunicode) 
-                                            if not chr(c) in [' ', ';', ',', '.'])
-
-    unicodePrintablesConference = ''.join(chr(c) for c in range(sys.maxunicode) 
-                                            if not chr(c) in [';', '.'])
-
-    non_abbreviated_name = Word(unicodePrintables)
-
-    name_component = (abbreviated_name | non_abbreviated_name)
-    author_name_after_comma = OneOrMore(name_component)
-    author_name_after_comma.setParseAction(makeDecoratingParseAction("author_name_after_comma"))
-
-    author = (author_name_before_comma("author_name_before_comma") + 
-                Literal(',').suppress() + 
-                author_name_after_comma("author_name_after_comma"))
-
-    author_list = delimitedList(author, delim = ';')
-
-    author.setParseAction(makeDecoratingParseAction("author"))
-
-    author_list = (delimitedList(author, delim = ';') + Literal('.').suppress()) 
-
-    sentence = (OneOrMore(Word(unicodePrintablesConference, excludeChars='.'), stopOn=Literal('. Em:')))
-    sentence.ignore('.')
-    
-    title = sentence('Title')
-    title = title.setParseAction(makeDecoratingParseAction("title"))
-
-    conference_name = Literal('Em: ').suppress() + OneOrMore(Word(unicodePrintablesConference, excludeChars='.,'), stopOn=Literal('.'))
-    conference = conference_name('conference_name') + Literal(',').suppress()
-    conference = conference.setParseAction(makeDecoratingParseAction("conference"))
-
-    year = Word(nums, exact=4) + Literal('.').suppress()
-    year = year.setParseAction(makeDecoratingParseAction("year"))
-
-    remaining_stuff = ZeroOrMore(Word(printables), stopOn=year)
-
-    valid_qualis = (Word(alphanums, exact=2) | Literal('C')) + ZeroOrMore(Word(unicodePrintablesConference, excludeChars='.'))
-    ni_qualis =  (Literal('Não identificado') + OneOrMore(Word(unicodePrintablesConference, excludeChars='.')))
+    ni_qualis =  (Literal('NÃO IDENTIFICADO') + OneOrMore(Word(printables, excludeChars='.')))
 
     qualis = (valid_qualis | ni_qualis + Literal('.').suppress())
     qualis = qualis.setParseAction(makeDecoratingParseAction("qualis"))
@@ -151,7 +79,7 @@ def parseConferenceRef(citation_str, debug = False):
         author_list.setName("author_list").setDebug()
         sentence.setName("Sentence").setDebug()
         title.setName("title").setDebug()
-        conference.setName("conference_name").setDebug()
+        conferjournal.setName("conference_journal").setDebug()
         year.setName("year").setDebug()
         remaining_stuff.setName("Remaining").setDebug()
         qualis.setName("qualis").setDebug()
@@ -159,7 +87,7 @@ def parseConferenceRef(citation_str, debug = False):
     citation = (author_list('AuthorLst') + 
                 title + 
                 Literal('.') + 
-                conference + 
+                conferjournal + 
                 remaining_stuff +
                 year +
                 qualis)
@@ -177,30 +105,24 @@ def infosCitation(result):
         elif element[0] == 'title':
             title = element[1].asList()
             title = ' '.join(word for word in title)
-        elif element[0] == 'journal':
-            journal = element[1].asList()
-            journal = ' '.join(word for word in journal)
-        elif element[0] == 'conference':
-            conference = element[1].asList()
-            conference = ' '.join(word for word in conference)
+        elif element[0] == 'conference_journal':
+            conference_journal = element[1].asList()
+            conference_journal = ' '.join(word for word in conference_journal)
         elif element[0] == 'Remaining':
             remaining = element[1].asList()
-            remaining = ' '.join(word for word in remaining)                
-            l = remaining.find('issn: ') + 6
-            issn = remaining[l:l+9]
+            remaining = ' '.join(word for word in remaining)
+            if 'ISSN' in remaining:                
+                l = remaining.find('ISSN: ') + 6
+                issn = remaining[l:l+9]
         elif element[0] == 'year':
             year = element[1][0]
         elif element[0] == 'qualis':
             qualis = element[1].asList()
             qualis = ' '.join(word for word in qualis)
 
-    try: return authors, title, journal, issn, year, qualis
-    except: return authors, title, conference, year, qualis
+    try: return authors, title, conference_journal, issn, year, qualis
+    except: return authors, title, conference_journal, year, qualis
 
-def parseJournalPublication(citation, debug = False):
+def parsePublication(citation, debug = False):
     result = parseRef(citation, debug = debug)
-    return infosCitation(result)
-
-def parseConferencePublication(citation, debug = False):
-    result = parseConferenceRef(citation, debug = debug)
     return infosCitation(result)
